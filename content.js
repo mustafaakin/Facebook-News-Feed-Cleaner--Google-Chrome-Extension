10c5aeee122e1468e285355b843c20a7e82d3d3f
Mustafa Akin <mustafa91@gmail.com>
2011-06-10 22:23:31 +0300

    Initial commit.

---
+++ content.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function(){
+	$(".uiList").bind("DOMSubtreeModified", function() {
+		chrome.extension.sendRequest({method: "getList"}, function(response) {
+			$('.pvm').each( function(){
+				var t = $(this).html();
+				var p = response.status;
+				if ( p != undefined){
+					var m = p.split("&");
+					for ( var i = 0; i < m.length; i++){
+						if ( m[i].length < 3) continue;
+						var mm = new RegExp(m[i], "i");
+						var p = t.match( mm);
+						if ( p!= null && p.length > 0){
+							$(this).fadeOut(500);		
+							break;
+						}							
+					}
+				}				
+			});
+		});
+	});
+});
---
+++ popup.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function(){
+	load();
+
+	$("#s").click(function(){
+		var t = $("#newBlock").val();
+		var p = "<a href='#' class='r'>"+ t + "</a><br/>";
+		$("#list").append(p);
+		$("#newBlock").val("");
+		save();
+	});
+	
+	function load(){
+		$("#list").html("");
+		var l = localStorage["list"];
+		if ( l != undefined){
+			var m = l.split("&");
+			for ( var i = 0; i < m.length; i++){
+				var p = "<a href='#' class='r'>"+ m[i] + "</a><br/>";
+				$("#list").append(p);
+			}
+		}
+	}
+	
+	function save(){
+		var k = "";
+		$(".r").each( function(){
+			var pp  = $(this).text();
+			if ( pp.length > 2)
+				k += pp + "&";
+		});
+		localStorage["list"] = k;
+	}
+	
+	chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+		if (request.method == "getList")
+			sendResponse({status: localStorage['list']});
+		else
+			sendResponse({}); // snub them.
+	});
+		
+	
+	$(".r").live('click', function(){
+		$(this).remove();
+		save();
+		load();
+	});
+});
